69e92a7e251f3a7c8915dde59479e4d44d79cef4
jiito <benjamin.allanrahill@gmail.com>
2023-01-02 10:44:03 -0500

    [main]: fix: styling

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -26,14 +26,15 @@ export default function Home() {
         <meta name="twitter:image" content="/Chad_contact.PNG" />
         <meta name="twitter:creator" content="@beeejar" />
       </Head>
-      <main className="flex m-auto flex-col font-bold text-4xl items-center ">
+      <main className="flex m-auto flex-col font-bold text-3xl items-center ">
         <h1 className="mt-20 mb-10">
           TEXT <span className="blue">&quot;WHAT&apos;S UP?&quot;</span> TO{" "}
         </h1>
         <h1> +1 (888) 493-CHAD</h1>
+        <h3 className="text-lg text-gray-500"> +1 (888) 493-2423</h3>
         <Image
-          width={400}
-          height={400}
+          width={300}
+          height={300}
           className="m-12 rounded-2xl"
           src="/Chad_contact.PNG"
           alt=""
